bee1a8fa79509935f0cf66b7ea719cc2b9801d3c
kmendezh <kendallmh2@hotmail.com>
2021-03-03 00:53:18 +0000

    Traffic Light, deliverable - K.Mendez

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -14,24 +14,49 @@ const trafficLightBox = {
 };
 
 export function TrafficLight() {
-	const updateLightState = () => {
-		setRedLight();
+	const updateRedLightState = () => {
+		setRedLight("0px 0px 80px red");
+		setYellowLight("none");
+		setGreenLight("none");
+	};
+
+	const updateYellowLightState = () => {
+		setYellowLight("0px 0px 80px yellow");
+		setRedLight("none");
+		setGreenLight("none");
+	};
+
+	const updateGreenLightState = () => {
+		setGreenLight("0px 0px 80px green");
+		setRedLight("none");
+		setYellowLight("none");
 	};
 
 	let [redLight, setRedLight] = useState("none");
+	let [yellowLight, setYellowLight] = useState("none");
+	let [greenLight, setGreenLight] = useState("none");
 	return (
 		<div style={trafficLightBox}>
 			<button
+				style={{ boxShadow: redLight }}
 				type="button"
 				onClick={() => {
-					updateLightState();
+					updateRedLightState();
 				}}
 				className="w-75 h-100 mx-auto mt-3 mb-3 rounded-circle bg-danger "></button>
 			<button
+				style={{ boxShadow: yellowLight }}
 				type="button"
+				onClick={() => {
+					updateYellowLightState();
+				}}
 				className="w-75 h-100 mx-auto mb-3 rounded-circle bg-warning"></button>
 			<button
+				style={{ boxShadow: greenLight }}
 				type="button"
+				onClick={() => {
+					updateGreenLightState();
+				}}
 				className="w-75 h-100 mx-auto mb-3 rounded-circle bg-success"></button>
 		</div>
 	);
